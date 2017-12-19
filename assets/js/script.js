cb774db2a8457cbf4c87a1889d0d830b1296b12f
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-12-19 09:28:14 +0100

    introduce mail signup form

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+console.log(navigator.appVersion);
+console.log('Your OS: '+OSName);
+console.log('https://github.com/meetalva/alva/releases/latest/')
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+httpGet('github.com/meetalva/alva/releases/latest/');

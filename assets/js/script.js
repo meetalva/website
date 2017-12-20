64cb6ccd5d311c0d0e5f039de592a377f2d21b65
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-12-20 17:20:42 +0100

    feat: automatically get latest release

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,45 @@
-console.log('https://github.com/meetalva/alva/releases/latest/')
-
 function getOS() {
-	var button=document.getElementById("download-btn");
-	var OSLink="https://github.com/meetalva/alva/releases/download/v0.5.0/";
-	var buttonText=button.innerHTML;
-	if (navigator.appVersion.indexOf("Win")!=-1) {
-		OSLink += "Alva-0.5.0.exe";
-		buttonText += " for Windows";
-	}
-	if (navigator.appVersion.indexOf("Mac")!=-1) {
-		OSLink += "Alva-0.5.0.dmg";
-		buttonText += " for macOS";
-	}
-	if (navigator.appVersion.indexOf("X11")!=-1) OSLink="UNIX"; //Unix
-	if (navigator.appVersion.indexOf("Linux")!=-1) OSLink="Linux"; //Linux
+	var OS = "undefinedOS";
+	if (navigator.appVersion.indexOf("Win")!=-1) OS={name:"Windows", file: ".exe"};
+	if (navigator.appVersion.indexOf("Mac")!=-1) OS={name: "macOS", file: ".dmg"};
 
-	console.log(OSLink);
-	button.href=OSLink;
-	button.innerHTML=buttonText;
+	return OS;
+}
+
+function latestOSRelease(os) {
+	var request = new XMLHttpRequest();
+	var latest;
+	var operatingSystem = os;
+	var url;
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4) {
+			latest = JSON.parse(request.response);
+			for (let i of latest.assets) {
+				if (i.name.indexOf(operatingSystem) !== -1){
+					console.log("latestOSRelease: "+ i.browser_download_url);
+					url = i.browser_download_url;
+				}
+			}
+		}
+	}
+
+	request.open('GET', 'https://api.github.com/repos/meetalva/alva/releases/latest', true);
+	request.send(null);
+
+	return url;
+}
+
+function generateDownloadLink() {
+	var button=document.getElementById("download-btn");
+	var buttonText=button.innerHTML;
+	var os = getOS();
+	var url = latestOSRelease(os.file);
+	
+	button.href=url;
+	button.innerHTML=buttonText + " for " + os.name;
 }
 
 window.onload = function(){
-	getOS();
+	generateDownloadLink();
 }

bb52bded036a5c1e5c11f83b08584ddefc584adb
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-12-20 19:09:21 +0100

    fix(index.html): fetch download link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,34 @@
-function getOS() {
-	var OS = "undefinedOS";
-	if (navigator.appVersion.indexOf("Win")!=-1) OS={name:"Windows", file: ".exe"};
-	if (navigator.appVersion.indexOf("Mac")!=-1) OS={name: "macOS", file: ".dmg"};
+window.onload = function() {
+  const os = getOS();
+  latestOSRelease(os);
+};
 
-	return OS;
+function getOS() {
+  var OS = "undefinedOS";
+  if (navigator.appVersion.indexOf("Win") != -1)
+    OS = { name: "Windows", file: ".exe" };
+  if (navigator.appVersion.indexOf("Mac") != -1)
+    OS = { name: "macOS", file: ".dmg" };
+
+  return OS;
 }
 
 function latestOSRelease(os) {
-	var request = new XMLHttpRequest();
-	var latest;
-	var operatingSystem = os;
-	var url;
-
-	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
-			latest = JSON.parse(request.response);
-			for (let i of latest.assets) {
-				if (i.name.indexOf(operatingSystem) !== -1){
-					console.log("latestOSRelease: "+ i.browser_download_url);
-					url = i.browser_download_url;
-				}
-			}
-		}
-	}
-
-	request.open('GET', 'https://api.github.com/repos/meetalva/alva/releases/latest', true);
-	request.send(null);
-
-	return url;
+  fetch("https://api.github.com/repos/meetalva/alva/releases/latest")
+    .then(response => response.json())
+    .then(data => {
+      for (let i of data.assets) {
+        if (i.name.indexOf(os.file) !== -1) {
+          generateDownloadLink(i.browser_download_url, os);
+          return;
+        }
+      }
+    });
 }
 
-function generateDownloadLink() {
-	var button=document.getElementById("download-btn");
-	var buttonText=button.innerHTML;
-	var os = getOS();
-	var url = latestOSRelease(os.file);
-	
-	button.href=url;
-	button.innerHTML=buttonText + " for " + os.name;
-}
-
-window.onload = function(){
-	generateDownloadLink();
+function generateDownloadLink(url, os) {
+  var button = document.querySelector(".js-download-button");
+  var buttonText = button.innerHTML;
+  button.href = url;
+  button.innerHTML = buttonText + " for " + os.name;
 }
